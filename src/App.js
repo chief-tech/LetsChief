1f49b4d34101660733b7f16fff669a600802cb0d
prakarshpandey <ppandey@g.hmc.edu>
2017-07-12 20:21:46 -0700

    initial commit leggo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => (
+  <View>
+    <Text>
+      This is the App
+    </Text>
+  </View>
+)
+
+export default App
