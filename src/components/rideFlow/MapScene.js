0ddfbbdd3825fe5cc9d0d04419e25449a676163f
prakarshpandey <ppandey@g.hmc.edu>
2017-07-16 20:34:35 -0700

    set up current location specifics for iOS

--- src/components/rideFlow/MapScene.js
+++ src/components/rideFlow/MapScene.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import  MapView  from 'react-native-maps'
+import  MapView, { PROVIDER_GOOGLE }  from 'react-native-maps'
 
-const LATITUDE_DELTA = 0
-const LONGITUDE_DELTA = 0
+const LATITUDE_DELTA = 0.1
+const LONGITUDE_DELTA = 0.1
 
 export default class MapScene extends React.Component {
   constructor (props) {
@@ -17,14 +17,44 @@ export default class MapScene extends React.Component {
     }
   }
 
+  onRegionChange = (region) => {
+    console.log(region)
+    this.setState({ region })
+  }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition( position => {
+      this.setState({
+        region:{
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA
+        }
+      })
+    })
+  }
+
   render () {
-   return (
-     <MapView
-       style={styles.mapStyle}
-       initialRegion={this.state.region}
-       onRegionChange={() => {}}
-     />
-   )
+    var marker = {
+      latLng: {
+        latitude: this.state.region.latitude,
+        longitude: this.state.region.longitude
+      }
+    }
+    console.log('Marker', marker)
+    console.log('Map state', this.state)
+    return (
+      <MapView
+        style={styles.mapStyle}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange} >
+        <MapView.Marker
+          coordinate={marker.latLng}
+          title={'Your current position'}
+          />
+      </MapView>
+    )
   }
 }
 
