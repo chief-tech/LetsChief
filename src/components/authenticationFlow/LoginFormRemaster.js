8291be338d76c52fe9b2b613ba2bdb4808a11fa3
prakarshpandey <ppandey@g.hmc.edu>
2017-07-30 17:46:52 -0700

    make login form look nice

--- src/components/authenticationFlow/LoginFormRemaster.js
+++ src/components/authenticationFlow/LoginFormRemaster.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Card, CardSection, Input, ButtonCommon, Spinner, ErrorModal } from '../common/index'
-import { Text, View, Image, TouchableOpacity } from 'react-native'
+import { View, Image, TouchableOpacity, Dimensions } from 'react-native'
+import { FormLabel, FormInput, FormValidationMessage, Button, SocialIcon, Icon } from 'react-native-elements'
 import firebase from 'firebase'
+
+const HEIGHT = Dimensions.get('window').height
+const WIDTH = Dimensions.get('window').width
 
 class LoginForm extends React.Component {
   static navigationOptions = {
@@ -70,7 +74,31 @@ class LoginForm extends React.Component {
       <Image source={require('../../../assets/images/Background.png')}
              style={styles.backgroundImage}
              resizeMode={'stretch'}>
+        <View style={styles.containerStyle}>
+          <FormLabel labelStyle={{color: '#007aff'}}>Email</FormLabel>
+          <FormInput onChangeText={this.onChangeEmail}/>
+          <FormValidationMessage>{this.props.error}</FormValidationMessage>
+          <FormLabel labelStyle={{color: '#007aff'}}>Password</FormLabel>
+          <FormInput onChangeText={this.onChangePassword}/>
+          <FormValidationMessage>{this.props.error}</FormValidationMessage>
+          <Button raised
+                  rounded
+                  title={"Let's Chief"}
+                  backgroundColor={'rgba(0, 122, 255, 0.2)'}
+                  component={TouchableOpacity}
+                  onPress={() => {}}
+                  textStyle={{fontSize: 20}}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <SocialIcon
+            style={styles.socialIcons}
+            type="facebook"/>
 
+          <SocialIcon
+            style={styles.socialIcons}
+            type="twitter"/>
+        </View>
       </Image>
     )
   }
@@ -80,6 +108,19 @@ const styles = {
   backgroundImage: {
     flex: 1,
     width: null,
+  },
+  containerStyle: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    paddingTop: HEIGHT / 10
+  },
+  iconContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  socialIcons: {
+    opacity: 0.6
   }
 }
 
